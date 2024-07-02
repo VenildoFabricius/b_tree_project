f7a8305a57bba23fdaf13d98aedff3af687f3814
Venildo Fabricius <venildofabricius@hotmail.com>
2024-07-02 16:59:10 -0300

    Remoção e balanceamento da remoção

--- btree.c
+++ btree.c
@@ -9,7 +9,7 @@ b_tree *cria_b_tree(int ordem){
     arv->ordem = ordem;
     // Aloca a sentinela
     arv->sentinela = (no*) malloc(sizeof(no));
-    // Aloca o vetor do filho da sentinela, com tamanho único
+    // Aloca o keyor do filho da sentinela, com tamanho único
     arv->sentinela->filhos = (no**) malloc(sizeof(no*));
     // Como a árvore é inicialmente vazia, sentinela não possui filho
     arv->sentinela->filhos[0] = NULL;
@@ -40,9 +40,9 @@ int insere_b_tree(b_tree *arv, int indice, int valor) {
         // Aloca as chaves do nó, com ordem chaves. A posição extra permite alocar uma chave além da permitida e
         // realizar a operação de split posteriormente
         raiz->key = (chave *)malloc(arv->ordem * sizeof(chave));
-        // Aloca o vetor de filhos, com tamanho ordem + 1. Mesmo caso da quantidade de chaves.
+        // Aloca o keyor de filhos, com tamanho ordem + 1. Mesmo caso da quantidade de chaves.
         raiz->filhos = (no **)malloc((arv->ordem + 1) * sizeof(no *));
-        // Para cada posição do vetor de filhos, atribui NULL, pois a raiz ainda não nenhum filho
+        // Para cada posição do keyor de filhos, atribui NULL, pois a raiz ainda não nenhum filho
         for (i = 0; i <= arv->ordem; i++) {
             raiz->filhos[i] = NULL;
         }
@@ -68,7 +68,7 @@ int insere_b_tree(b_tree *arv, int indice, int valor) {
         for (i = 0; i < atual->n; i++) {
             // Compara as chaves do nó atual até encontrar uma chave com índice maior do que o índice do nó a ser inserido
             if (indice < atual->key[i].indice) {
-                // Encontrou o vetor de filhos onde a inserção deve ser feita
+                // Encontrou o keyor de filhos onde a inserção deve ser feita
                 atual = atual->filhos[i];
                 achou = 1;
                 // Sai do loop
@@ -78,7 +78,7 @@ int insere_b_tree(b_tree *arv, int indice, int valor) {
         // Se o índice da chave a ser inserida for maior que o maior índice do nó atual, a chave pertence a um filho
         // direito do nó atual e o loop anterior não pôde encontrar
         if (!achou) {
-            // A chave deve ser inserida no vetor com chaves maiores que o maior índice do nó atual
+            // A chave deve ser inserida no keyor com chaves maiores que o maior índice do nó atual
             atual = atual->filhos[atual->n];
         }
     }
@@ -91,7 +91,7 @@ int insere_b_tree(b_tree *arv, int indice, int valor) {
         if (indice > atual->key[i - 1].indice) {
             break;
         }
-        // Se não, os índices são copiados uma posição para a frente do vetor até que a posição de inserção seja encontrada
+        // Se não, os índices são copiados uma posição para a frente do keyor até que a posição de inserção seja encontrada
         atual->key[i] = atual->key[i - 1];
     }
     // Encontrada a posição de inserção, atribui-se o índice e o valor passados como parâmetros à respectiva chave
@@ -197,9 +197,9 @@ void bal_insercao(b_tree *arv, no *atual) {
     } else {
         pai = atual->pai;
         for (i = pai->n; i > atual->pai_posicao; i--) {
-            // Os índices são copiados uma posição para a frente do vetor a cada iteração
+            // Os índices são copiados uma posição para a frente do keyor a cada iteração
             pai->key[i] = pai->key[i - 1];
-            // A partir da posição atual do nó, os vetores dos filhos do pai são copiados uma posição para a frente
+            // A partir da posição atual do nó, os keyores dos filhos do pai são copiados uma posição para a frente
             pai->filhos[i + 1] = pai->filhos[i];
             // E a sua posição em relação ao pai é incrementada
             pai->filhos[i + 1]->pai_posicao++;
@@ -269,3 +269,269 @@ void bal_insercao(b_tree *arv, no *atual) {
         arv->quantidadeNos++;
     }
 }
+
+int remove_b_tree(b_tree *arv, int indice) {
+    if (!arv|| !arv->sentinela->filhos[0])
+        return 0;
+
+    int pos = -1, i;
+    no *atual = arv->sentinela->filhos[0];
+
+    // Procura indice na B Tree
+    do {
+        for (i = 0; i < atual->n; i++) {
+            if (indice == atual->key[i].indice) {
+                // pos armazena posição do elemento no nó
+                pos = i;
+                break;
+            }
+
+            if (indice < atual->key[i].indice) {
+                // Caso não ache no vetor, nó atual recebe um de seus filhos
+                atual = atual->filhos[i];
+                i = -1;
+                break;
+            }
+        }
+
+        // Caso indice seja maior que todos os elementos do vetor, atual recebe último
+        // ponteiro do nó
+        if (atual != NULL && i == atual->n)
+            atual = atual->filhos[i];
+    } while (pos == -1 && atual != NULL);
+
+    // Caso não tenha encontrado indice na árvore
+    if (atual == NULL)
+        return 0;
+
+    if (atual->folha) { // Caso indice a ser removido esteja em um nó folha
+        // Copia todos os valores uma posição para frente
+        for (i = pos; i < atual->n - 1; i++) {
+            atual->key[i] = atual->key[i + 1];
+        }
+        atual->n--;
+
+        // Verifica se houve desbalanceamento
+        if (atual->n < (arv->ordem - 1) / 2 &&
+            arv->sentinela->filhos[0] != atual) {
+            bal_remocao(arv, atual);
+        } else if (atual->n ==
+                   0) { // Significa que não existe mais elementos na árvore
+            // Libera estruturas do nó atual
+            free(atual->key);
+            free(atual->filhos);
+            free(atual);
+
+            arv->sentinela->filhos[0] = NULL;
+            arv->quantidadeNos--;
+        }
+    } else { // Caso indice esteja em um nó interno da árvore
+        // Procura sucessor do valor
+        no *filho = atual->filhos[pos + 1];
+        while (filho->filhos[0])
+            filho = filho->filhos[0];
+
+        // Copia valor do sucessor para lugar do indice removido
+        atual->key[pos] = filho->key[0];
+
+        // Copia valores uma posição para frente no nó filho
+        for (i = 0; i < filho->n - 1; i++) {
+            filho->key[i] = filho->key[i + 1];
+        }
+        filho->n--;
+
+        // Verifica se houve desbalanceamento no nó filho
+        if (filho->n < (arv->ordem - 1) / 2) {
+            bal_remocao(arv, filho);
+        }
+    }
+
+    return 1;
+}
+
+void bal_remocao(b_tree *arv, no *atual) {
+    int esq = 0, dir = 0;
+    no *irmao;
+    no *pai = atual->pai;
+
+    // Verifica se é possível fazer rotação com irmão
+    if (atual->pai_posicao > 0 && pai->filhos[atual->pai_posicao - 1] != NULL &&
+        pai->filhos[atual->pai_posicao - 1]->n >
+        (arv->ordem - 1) / 2) { // Indica que é possível fazer rotação esq
+        esq = 1;
+        irmao = pai->filhos[atual->pai_posicao - 1];
+    } else if (pai->n >= atual->pai_posicao + 1 &&
+               pai->filhos[atual->pai_posicao + 1]->n >
+               (arv->ordem - 1) /
+               2) { // Indica que é possível fazer rotação dir
+        dir = 1;
+        irmao = pai->filhos[atual->pai_posicao + 1];
+    }
+
+    if (esq) { // Rotação com irmão da esquerda
+        // Copia valores e filhos do nó atual uma posição para trás
+        for (int i = atual->n; i > 0; i--) {
+            atual->key[i] = atual->key[i - 1];
+            atual->filhos[i + 1] = atual->filhos[i];
+            if (atual->filhos[i + 1] != NULL)
+                atual->filhos[i + 1]->pai_posicao++;
+        }
+        atual->filhos[1] = atual->filhos[0];
+        if (atual->filhos[1] != NULL)
+            atual->filhos[1]->pai_posicao++;
+
+        // Copia valor do nó pai para a primeira posição do nó atual
+        atual->key[0] = pai->key[atual->pai_posicao - 1];
+
+        // Copia valor da última posição do irmão para a posição do pai que foi
+        // copiada para nó atual
+        pai->key[atual->pai_posicao - 1] = irmao->key[irmao->n - 1];
+
+        // Muda pai do nó que estava na última posição do vetor do irmão
+        atual->filhos[0] = irmao->filhos[irmao->n];
+        if (atual->filhos[0] != NULL) {
+            atual->filhos[0]->pai = atual;
+            atual->filhos[0]->pai_posicao = 0;
+        }
+        atual->n++;
+        irmao->n--;
+    } else if (dir) { // Rotação com irmão da direita
+        // Copia valor do pai para última posição do vetor do nó atual
+        atual->key[atual->n] = pai->key[atual->pai_posicao];
+
+        // Copia valor da primeira posição do irmão para o pai
+        pai->key[atual->pai_posicao] = irmao->key[0];
+
+        // Copia primeiro ponteiro do irmão para última posição do nó atual
+        atual->filhos[atual->n + 1] = irmao->filhos[0];
+        if (atual->filhos[atual->n + 1] != NULL) {
+            atual->filhos[atual->n + 1]->pai = atual;
+            atual->filhos[atual->n + 1]->pai_posicao = atual->n + 1;
+        }
+
+        // Move elementos do nó irmao uma posição para frente
+        for (int i = 0; i < irmao->n - 1; i++) {
+            irmao->key[i] = irmao->key[i + 1];
+            irmao->filhos[i] = irmao->filhos[i + 1];
+            if (irmao->filhos[i] != NULL)
+                irmao->filhos[i]->pai_posicao--;
+        }
+        irmao->filhos[irmao->n - 1] = irmao->filhos[irmao->n];
+        if (irmao->filhos[irmao->n - 1] != NULL)
+            irmao->filhos[irmao->n - 1]->pai_posicao--;
+
+        atual->n++;
+        irmao->n--;
+    } else {                        // Operação de Merge
+        if (atual->pai_posicao < pai->n) { // Merge com irmão da direita
+            irmao = pai->filhos[atual->pai_posicao + 1];
+
+            // Copia valor do pai para última posição do nó atual
+            atual->key[atual->n] = pai->key[atual->pai_posicao];
+
+            // Copia valores do pai uma posição para frente a partir da posição que
+            // foi para nó atual
+            for (int i = atual->pai_posicao + 1; i < pai->n; i++) {
+                pai->key[i - 1] = pai->key[i];
+                pai->filhos[i] = pai->filhos[i + 1];
+                if (pai->filhos[i] != NULL)
+                    pai->filhos[i]->pai_posicao--;
+            }
+
+            // Copia valores do irmão para o nó atual
+            int j = atual->n + 1;
+            for (int i = 0; i < irmao->n; i++) {
+                atual->key[j] = irmao->key[i];
+                atual->filhos[j] = irmao->filhos[i];
+                if (atual->filhos[j] != NULL) {
+                    atual->filhos[j]->pai = atual;
+                    atual->filhos[j]->pai_posicao = j;
+                }
+                j++;
+            }
+
+            // Copia último ponteiro do irmão para o nó atual
+            atual->filhos[j] = irmao->filhos[irmao->n];
+            if (atual->filhos[j] != NULL) {
+                atual->filhos[j]->pai = atual;
+                atual->filhos[j]->pai_posicao = j;
+            }
+
+            // Atual recebeu todos os nós de seu irmão e um de seu pai
+            atual->n += irmao->n + 1;
+
+            // Libera estruturas do irmão
+            free(irmao->key);
+            free(irmao->filhos);
+            free(irmao);
+
+            arv->quantidadeNos--;
+
+            // Indica qual nó foi removido o irmão ou o nó atual
+            esq = 1;
+        } else { // Merge com irmão da esquerda
+            irmao = pai->filhos[atual->pai_posicao - 1];
+
+            // Copia valor do pai para última posição do nó irmão
+            irmao->key[irmao->n] = pai->key[atual->pai_posicao - 1];
+
+            // Copia todos os elementos do nó atual para final do nó irmão
+            int j = irmao->n + 1;
+            for (int i = 0; i < atual->n; i++) {
+                irmao->key[j] = atual->key[i];
+                irmao->filhos[j] = atual->filhos[i];
+                if (irmao->filhos[j] != NULL) {
+                    irmao->filhos[j]->pai = irmao;
+                    irmao->filhos[j]->pai_posicao = j;
+                }
+                j++;
+            }
+
+            // Copia último ponteiro do nó atual para o irmão
+            irmao->filhos[j] = atual->filhos[atual->n];
+            if (irmao->filhos[j] != NULL) {
+                irmao->filhos[j]->pai = irmao;
+                irmao->filhos[j]->pai_posicao = j;
+            }
+
+            // Irmão recebeu todos os nós de atual e um de seu pai
+            irmao->n += atual->n + 1;
+
+            // Libera estruturas do nó atual
+            free(atual->key);
+            free(atual->filhos);
+            free(atual);
+
+            arv->quantidadeNos--;
+        }
+
+        // Na operação de Merge o pai sempre perde um elemento
+        pai->n--;
+
+        // Verifica se desbalanceou o pai
+        if (pai->n < (arv->ordem - 1) / 2 && arv->sentinela->filhos[0] != pai) {
+            bal_remocao(arv, pai);
+        } else if (pai->n == 0) {
+            // Caso a raiz da árvore tenha ficado com 0 elementos é necessário fazer
+            // atualização
+
+            // Verifica qual nó deve se tornar a nova raiz
+            if (esq) {
+                arv->sentinela->filhos[0] = atual;
+                atual->pai = arv->sentinela;
+            } else {
+                arv->sentinela->filhos[0] = irmao;
+                irmao->pai = arv->sentinela;
+            }
+
+            // Libera estruturas da antiga raiz
+            free(pai->key);
+            free(pai->filhos);
+            free(pai);
+
+            arv->quantidadeNos--;
+        }
+    }
+}
+
+
